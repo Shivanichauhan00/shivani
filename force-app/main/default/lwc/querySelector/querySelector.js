520eb101f952b7636e99ad3ed09f824dc2268d18
Shivanichauhan00 <165651539+Shivanichauhan00@users.noreply.github.com>
2024-04-01 14:54:29 +0530

    third commit

--- force-app/main/default/lwc/querySelector/querySelector.js
+++ force-app/main/default/lwc/querySelector/querySelector.js
@@ -1,7 +1,7 @@
 import { LightningElement } from 'lwc';
 
 export default class QuerySelector extends LightningElement {
-    //hgdhtd
+    
     fetchAllDetails(){
        const elem = this.template.querySelector('h1');
        console.log(elem.innerText) ; 
